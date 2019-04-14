94b537a48112d89821632614b94bfc6a97a5c86e
Svetlin Svilenov <s.e.svilenov@gmail.com>
2019-04-14 19:05:42 +0300

    Initial commit + basic movement scripted

---
+++ Assets/LookAtPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LookAtPoint : MonoBehaviour
+{
+    [SerializeField] private Vector3 lookAtPoint = Vector3.zero;
+
+    void Update()
+    {
+        lookAtPoint = gameObject.transform.parent.transform.position;
+        transform.LookAt(lookAtPoint);
+    }
+}
---
+++ Assets/Mover.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    private int forwardSpeedId = Animator.StringToHash("ForwardSpeed");
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            MoveToCursor();
+
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        bool hasHit = Physics.Raycast(ray, out hitInfo, 100f);
+        if (hasHit)
+            agent.SetDestination(hitInfo.point);
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        float speed = localVelocity.z;
+
+        animator.SetFloat(forwardSpeedId, speed);
+    }
+}
