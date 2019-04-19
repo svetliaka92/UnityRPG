f11b5afefb7dd97b35f9e40c428f640a57085a80
svetliaka92 <s.e.svilenov@gmail.com>
2019-04-19 16:29:26 +0300

    Started working on enemy AI

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] private float chaseDistance = 5f;
+
+        private void Update()
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (Vector3.Distance(player.transform.position, transform.position) <= chaseDistance)
+                print(gameObject + "is rolling out!");
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+    }
+}
