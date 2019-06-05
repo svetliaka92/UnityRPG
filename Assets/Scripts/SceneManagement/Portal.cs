83ed29e9cab3dae67a755a1c313a47f10b25d50b
Svetlin Svilenov <s.e.svilenov@gmail.com>
2019-06-05 23:47:02 +0300

    Portal objects added to both scenes and scene loading hard coded. +Improvement

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private int sceneToLoad;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            print("Portal entered...");
+            if (other.tag == "Player")
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
